e3b8e8ac7845677e412d3ef6958e2e59eb437a3d
Anton Sukhadolets <suhodoleca@gmail.com>
2023-06-14 16:36:05 +0200

    feat: add number of mistakes functionality to modal window

--- src/js/components/modalWindow.js
+++ src/js/components/modalWindow.js
@@ -17,8 +17,12 @@ const createModalWindow = () => {
   text.classList.add('modal-window__text');
   text.textContent = '';
 
+  const h3 = createElement('h3');
+  h3.classList.add('modal-window__score');
+
   modalWindow.append(icon);
   modalWindow.append(text);
+  modalWindow.append(h3);
 
   return modalWindow;
 };
@@ -27,11 +31,17 @@ const showModalWindow = () => {
   const modalWindow = GET_VAR('modalWindow');
   const modalWindowIcon = GET_VAR('modalWindowIcon');
   const modalWindowText = GET_VAR('modalWindowText');
+  const modalWindowhScore = document.querySelector('.modal-window__score');
+  const scoreLine = GET_VAR('scoreLine');
   if (isGameOverSuccess()) {
     modalWindowIcon.src = GET_VAR('modalWindowIconWin');
     modalWindowText.textContent = 'You are win!';
     modalWindow.classList.add('appear');
+    modalWindowhScore.textContent = '';
   } else {
+    const scoreLineIcons = scoreLine.querySelectorAll('img');
+    const amountOfMistakes = Array.from(scoreLineIcons).filter((item) => item.src.indexOf('assets/images/score-icons/wrong.png') !== -1).length;
+    modalWindowhScore.textContent = `Mistakes: ${amountOfMistakes}`;
     modalWindowIcon.src = GET_VAR('modalWindowIconLose');
     modalWindowText.textContent = 'You are lose!';
     modalWindow.classList.add('appear');
